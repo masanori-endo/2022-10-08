01fbe37fe8f77e204e561b832878bc10915210ec
masanori-endo <ultima19740330@gmail.com>
2022-10-08 11:04:07 +0900

    update

---
+++ sample2.py
@@ -0,0 +1,12 @@
+def prime_numbers_v1(numbers):
+    prime = []
+    for x in range(2, numbers):
+        for y in range(2, x):
+            if x % y == 0:
+                break
+        else:
+            prime.append(x)
+    return prime
+
+print(prime_numbers_v1(100))
+
